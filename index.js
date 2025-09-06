60b121684e8fbc1a3b7109d89a60f8b33ea42559
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-07 01:51:49 +0600

    added function to category to show content

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
-const catagoryContainer = document.getElementById('catagoryContainer')
+const catagoryContainer = document.getElementById("catagoryContainer");
+const categoryContent = document.getElementById("categoryContent");
 
-const loadCategory = () =>{
-  fetch('https://openapi.programming-hero.com/api/categories')
-  .then(res => res.json())
-  .then(data => displayCategory
-    (data.categories))
-}
-const displayCategory = (categories) =>{
-  catagoryContainer.innerHTML = ''
-  categories.forEach(category => {
-    console.log(category.category_name)
-    catagoryContainer.innerHTML +=`
-    <li onclick="" class="font-medium text-[16px] hover:text-white hover:bg-[#15803D] w-full py-2 px-[10px] rounded-lg"
-            >
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategory(data.categories));
+};
+const displayCategory = (categories) => {
+  catagoryContainer.innerHTML = "";
+  categories.forEach((category) => {
+    catagoryContainer.innerHTML += `
+    <li id="${category.id}" class="font-medium text-[16px] hover:text-white hover:bg-[#15803D] w-full py-2 px-[10px] rounded-lg">
               ${category.category_name}
             </li>
-    `
+    `;
   });
-}
-loadCategory()
+  catagoryContainer.addEventListener("click", (e) => {
+    const allLi = document.querySelectorAll("#catagoryContainer li");
+    allLi.forEach((li) => {
+      li.classList.remove("text-white", "bg-[#15803D]");
+    });
+
+    if (e.target.localName === "li") {
+      e.target.classList.add("text-white", "bg-[#15803D]");
+      loadCategoryContent(e.target.id);
+    }
+  });
+};
+
+const loadCategoryContent = (categoryId) => {
+  fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
+    .then((res) => res.json())
+    .then((data) => showCategoryContent(data.plants));
+};
+
+const showCategoryContent = (contents) => {
+  categoryContent.innerHTML = "";
+  contents.forEach((content) => {
+    console.log(content);
+
+    categoryContent.innerHTML += `
+    <div class="card bg-base-100 w-[300px] shadow-sm mt-3">
+  <figure>
+    <img
+      class="w-[298px]"
+      src="${content.image}"
+    />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title font-semibold text-[17px] dark1">${content.name}</h2>
+    <p class="font-normal text-[13px] dark1">
+      ${content.description}
+    </p>
+    <div class="flex justify-between my-4">
+      <p class="green w-[86px]">
+        <span class="bg-[#DCFCE7] px-3 py-2 rounded-2xl font-semibold text-[14px]">
+          ${content.category}
+        </span>
+      </p>
+      <p class="text-right font-semibold dark1 text-[14px]">${content.price}</p>
+    </div>
+    <div class="card-actions">
+      <button class="btn btn-bg w-full rounded-3xl">Add to Cart</button>
+    </div>
+  </div>
+</div>
+    `;
+  });
+};
+loadCategory();
